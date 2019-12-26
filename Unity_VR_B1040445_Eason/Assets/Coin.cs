08e78b7a9ea6e9a27be3a56e447a85496c7b0592
zenja90907 <55218478+zenja90907@users.noreply.github.com>
2019-12-26 17:25:11 +0800

    完成(除了傳送陣)

---
+++ Unity_VR_B1040445_Eason/Assets/Coin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int speed;
+
+    private void Update()
+    {
+        transform.Rotate(0,0,speed * Time.deltaTime);
+    }
+}
--- Unity_VR_B1040445_Eason/Assets/Collet.cs
+++ Unity_VR_B1040445_Eason/Assets/Collet.cs
@@ -1,9 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Collet : MonoBehaviour
 {
+    public int playerScore;
+    public Text textsay;
+
    public void ThrowDetach(GameObject obj)
     {
         Destroy(obj);
+        if(obj.gameObject.tag=="Gold")
+        {
+            playerScore += 100;
+            textsay.text = "分數:"+playerScore.ToString();
+        }
+        else if(obj.gameObject.tag=="Sliver")
+        {
+            playerScore += 60;
+            textsay.text = "分數:"+playerScore.ToString();
+        }
+        
+       // if(obj.gameObject.tag=="Sliver")
+        //{
+          //  playerScore += 60;
+        //}
     }
 }
